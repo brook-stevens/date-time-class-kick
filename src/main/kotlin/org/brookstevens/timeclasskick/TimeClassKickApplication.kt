f79f957e9f27f5d24958674eda630cfbb0e39a93
Brook Stevens <brook@brookstevens.org>
2020-04-18 11:15:33 -0400

    Initial spring boot starter app

---
+++ src/main/kotlin/org/brookstevens/timeclasskick/TimeClassKickApplication.kt
@@ -0,0 +1,11 @@
+package org.brookstevens.timeclasskick
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeClassKickApplication
+
+fun main(args: Array<String>) {
+	runApplication<TimeClassKickApplication>(*args)
+}
