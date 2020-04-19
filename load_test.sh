4a2823829717edbbbca1483c19a240fcce08e25a
Brook Stevens <brook@brookstevens.org>
2020-04-19 10:31:34 -0400

    Clean up formatting and make ip of ec2 load driver dynamic in shell

--- load_test.sh
+++ load_test.sh
@@ -1,8 +1,9 @@
 ./gradlew testLoad -Pgatling-date-time-base-url="http://date-time-tf-477828465.us-east-2.elb.amazonaws.com"
+MY_IP=`dig +short myip.opendns.com @resolver1.opendns.com`
+REPORT_FOLDER=`ls -t build/gatling-results | head -1`
 echo ""
 echo ""
 echo "#####################################################"
 echo "If running from driver linux machine view results at:"
-REPORT_FOLDER=`ls -t build/gatling-results | head -1`
-echo "http://18.220.118.143/html/gatling/${REPORT_FOLDER}"
+echo "http://${MY_IP}/html/gatling/${REPORT_FOLDER}"
 echo "#####################################################"
--- src/test/scala/webservice/gatling/simulation/DateTimeWebServiceLoadTestSimulation.scala
+++ src/test/scala/webservice/gatling/simulation/DateTimeWebServiceLoadTestSimulation.scala
@@ -1,25 +1,24 @@
 package webservice.gatling.simulation
- 
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
- 
+
 class DateTimeWebServiceLoadTestSimulation extends Simulation {
- val baseUrl = System.getenv("gatling-date-time-base-url")
- if(baseUrl == null) {
-  throw new NullPointerException("System property gatling-date-time-base-url must be set")
- }
- val httpProtocol = http
-   .baseUrl(baseUrl)
-//   .acceptEncodingHeader("gzip, deflate")
+  val baseUrl = System.getenv("gatling-date-time-base-url")
+  if (baseUrl == null) {
+    throw new NullPointerException("System property gatling-date-time-base-url must be set")
+  }
+  val httpProtocol = http
+    .baseUrl(baseUrl)
 
- val scn = scenario("LoadTest")
-   .exec(http("request_date_time")
-     .get("/date-time"))
+  val scn = scenario("LoadTest")
+    .exec(http("request_date_time")
+      .get("/date-time"))
 
- setUp(scn.inject(constantUsersPerSec(1200) during (1 minutes))).throttle(
-  reachRps(1200) in (30 seconds),
-  holdFor(1 minute)
- ).protocols(httpProtocol)
+  setUp(scn.inject(constantUsersPerSec(1200) during (1 minutes))).throttle(
+    reachRps(1200) in (30 seconds),
+    holdFor(1 minute)
+  ).protocols(httpProtocol)
 }
